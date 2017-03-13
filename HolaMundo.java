8211949f9719d7bf71649a24f816e9e53ac03dbe
garcones <gema.ao@gmail.com>
2017-03-13 09:00:34 +0100

    Subida incial

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+package com.holamundo;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("Hola Mundo!...");  //Impresion en Consola
+         System.out.println("Bienvenido al HolaMundo!..."); //Impresion en Consola
+	}
+
+}
